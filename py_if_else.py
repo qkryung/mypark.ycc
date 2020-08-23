e01e0b1cf9825e770cc4023fc0dc3e108949bd16
mypark <yungao@LAPTOP-I70S6PP4.localdomain>
2020-08-23 18:10:23 +0900

    ycc2

---
+++ py_if_else.py
@@ -0,0 +1,27 @@
+#https://www.hackerrank.com/challenges/py_if_else/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+        n = int(input().strip())
+
+            if n % 2 == 1:
+                        print("Weird")
+
+                            if n % 2 == 0 and 2 <= n and n <= 5:
+                                        print("Not Weird")
+
+                                            if n % 2 == 0 and 6 <= n and n <= 20:
+                                                        print("Weird")
+
+                                                            if n % 2 == 0 and n > 20:
+                                                                        print("Not Weird")
+
