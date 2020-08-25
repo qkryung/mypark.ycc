8bdad538754ba76da889fa1c40805c8803bc3232
mypark <yungao@LAPTOP-I70S6PP4.localdomain>
2020-08-25 20:12:40 +0900

    ycc2

--- polar_coordinates.py
+++ polar_coordinates.py
@@ -1,3 +1,4 @@
+#https://www.hackerrank.com/challenges/polar_coordinates/problem
 #Enter your code here. Read input from STDIN. Print output to STDOUT
 
 import cmath
---
+++ python_mod_divmod.py
@@ -0,0 +1,10 @@
+#https://www.hackerrank.com/challenges/python_mod_divmod/proble
+
+# Enter your code here. Read input from STDIN. Print output to STDT
+
+a = int(input())
+b = int(input())
+
+print(a // b)
+print(a % b)
+print(divmod(a, b))
