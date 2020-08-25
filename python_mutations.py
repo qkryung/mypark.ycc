651190e850dbc6c5081cb4faa33943b5dd32d6da
mypark <yungao@LAPTOP-I70S6PP4.localdomain>
2020-08-25 21:05:41 +0900

    ycc2

---
+++ python_mutations.py
@@ -0,0 +1,15 @@
+#https://www.hackerrank.com/challenges/python_mutations/problem
+
+
+def mutate_string(string, position, character):
+    a = list(string)
+    a[position] = character
+    string = ''.join(a)
+    return (string)
+
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
