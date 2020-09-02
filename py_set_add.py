0da7e1fcb04f45b2ec771d1dd0cfd114067783a7
mypark <yungao@LAPTOP-I70S6PP4.localdomain>
2020-09-02 17:56:48 +0900

    ycc3

---
+++ py_set_add.py
@@ -0,0 +1,8 @@
+#https://www.hackerrank.com/challenges/py_set_add/problem
+
+#Read input from STDIN. Print output to STDOUT
+
+s = set()
+for i in range(int(input())):
+    s.add(input())
+print(len(s))
